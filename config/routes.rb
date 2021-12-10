4b46e63d658fb2139c01852baffa7179cdffe003
tuan <maidinhtuan123456@gmail.com>
2021-12-10 09:45:36 +0000

    Edit password

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root to:"main#index"
   get "sign_up", to:"registrations#new"
   post "sign_up", to:"registrations#create"
+  
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+  get "sign_in", to:"session#new"
+  post "sign_in", to:"session#create"
+  
   delete "logout", to: "session#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_095329) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
